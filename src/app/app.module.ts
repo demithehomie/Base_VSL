0c0635031289c12794ff7e2457cf991553535279
Demetrius Ferreira <demithehomie@gmail.com>
2023-05-13 11:33:18 -0300

    Vimeo video added with autoplay, no pause, and no progress bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { YouTubePlayerModule } from '@angular/youtube-player';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -9,8 +10,9 @@ import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, YouTubePlayerModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
